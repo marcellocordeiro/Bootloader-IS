a1559a5850b98ed63f5daee468b536cb901db998
Marcello Santiago Cordeiro <msc3@ug2c10.windows.cin.ufpe.br>
2017-04-10 10:31:08 -0300

    fml

--- boot2.asm
+++ boot2.asm
@@ -1,117 +1,97 @@
 org 0x500
 jmp 0x0000:start
-    dados:
-        string1: db 'Loading structures for the kernel...', 0
-        string2: db 'Setting up protected mode...', 0
-        string3: db 'Loading kernel in memory...', 0
-        string4: db 'Running kernel...', 0
+
+string1: db 'Loading structures for the kernel...', 13, 10, 0
+string2: db 'Setting up protected mode...', 13, 10, 0
+string3: db 'Loading kernel in memory...', 13, 10, 0
+string4: db 'Running kernel...', 13, 10, 0
+
 start:
-    xor ax, ax
-    mov ds, ax
+	xor ax, ax
+	mov ds, ax
 
-    mov ah, 0
-    mov al, 13                      ;Entra no modo video
-    int 10h
+	;video mode
+	mov ah, 00h
+	mov al, 12h
+	int 10h
 
-    call espera
-    mov ah, 2
-    mov bh, 0                           ;move cursor
-    mov dh, 3
-    mov dl, 0
-    int 10h
-    mov si, string1                     ;printa string1
-    call printString
-    call espera
+	;background
+	;mov ah, 0bh
+	;mov bh, 00h
+	;mov bl, 00h
+	;int 10h
 
-    mov ah, 2
-    mov bh, 0                           ;Move cursor
-    mov dh, 5
-    mov dl, 0
-    int 10h
-    mov si, string2                     ;Printa string2
-    call printString
-    call espera
+	;set palette
+	mov ah, 0bh
+	mov bh, 01h
+	mov bl, 00h
+	int 10h
 
-    mov ah, 2
-    mov bh, 0
-    mov dh, 7                           ;Move cursor
-    mov dl, 0
-    int 10h
-    mov si, string3                    ;Printa string3
-    call printString
-    call espera
+	mov si, string1
+	call printString ; imprimindo a primeira mensagem
+	call delay
+	
+	mov si, string2
+	call printString ; imprimindo a segunda mensagem
+	call delay
 
-    mov ah, 2
-    mov bh, 0
-    mov dh, 9                           ;move cursor
-    mov dl, 0
-    int 10h
-    mov si, string4                     ;Printa string4
-    call printString
+	mov si, string4
+	call printString
+	call delay
+	
+	mov si, string3
+	call printString ; imprimindo terceira mensagem
+	call delay
 
-    call espera
-    call espera
+	reset:
+		mov ah, 0
+		mov dl, 0
+		int 13h
+		jc reset
 
-    reset:
-        mov ah, 0
-        mov dl, 0
-        int 13h
-        jc reset
+		mov ax, 0x7e0
+		mov es, ax
+		xor bx, bx
 
-        mov ax, 0x7e0
-        mov es, ax
-        xor bx, bx
+		ler:
+			mov ah, 0x02
+			mov al, 6
+			mov ch, 0
+			mov cl, 3
+			mov dh, 0
+			mov dl, 0
+			int 13h
+			jc ler
 
-        ler:
-            mov ah, 0x02
-            mov al, 6
-            mov ch, 0
-            mov cl, 3
-            mov dh, 0
-            mov dl, 0
-            int 13h
-            jc ler
+			jmp 0x0000:0x7e00
 
-            jmp 0x0000:0x7e00
+delay:
+	mov ah, 86h
+	mov cx, 20
+	xor dx, dx
+	mov dx, 40
+	;int 15h
+	
+	ret
 
-  espera:                                           ;Macro que espera 1 segundo
-    mov ah, 86h
-    mov cx, 10
-    mov dx, 0
-    int 15h
+printString:
+	lodsb ;carrega um caractere e passa o ponteiro para o proximo / Carrega um byte de DS:SI em AL e depois incrementa SI 
 
-    ret
+	cmp al, 0 ;0 é o código do \0
+	je .done ;se cmp for verdadeiro (verifica no registrador de flags)
 
-  printString:                              ;Macro que printa a string localizada em si
-     mov cl, 0
-     imprime:
-        lodsb
-        cmp al, cl
-        je feito
+	mov ah, 0eh ;imprime o caractere de al
+	mov bh, 00h
+	mov bl, 03h ;cor do caractere (modo grafico)
+	int 10h
 
-        mov ah, 0xe
-        mov bh, 0
-        mov bl, 3
-        int 10h
-        jmp imprime
+	mov ah, 86h
+	mov cx, 1
+	xor dx, dx
+	;mov dx, 2	
+	;int 15h
 
-      feito:
-        ret
+	jmp printString
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	.done:
+		ret
